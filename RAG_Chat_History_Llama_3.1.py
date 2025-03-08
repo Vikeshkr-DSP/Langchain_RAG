d213d630317aa9dc82593a45aaab8c6d68f7ecc9
Vikesh kumar <vikeshkr875@gmail.com>
2025-03-08 09:41:52 +0530

    code optimization and adding requirementa.txt

--- RAG_Chat_History_Llama_3.1.py
+++ RAG_Chat_History_Llama_3.1.py
@@ -1,7 +1,6 @@
 import os
-import time
 import streamlit as st
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS  
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -13,53 +12,53 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 
-## Setting-up Streamlit
-st.title('RAG with PDF with message history')
-st.write('Upload PDF')
+## Building UI
+st.title('Retrieval-Augmented Generation (RAG) with Chat History')
+st.sidebar.title('Configuration')
+st.sidebar.write('Last 5 conversation messages are used to build the query context')
+session_id = st.sidebar.text_input(label='Change the session ID as required', value='Default')
 
-## Configuration required for LLM with chat histiry
+## Configuration required for LLM, Embedding and text splitting
 model = ChatOllama(model='llama3.1', temperature=0.5)
 embedng_techn = OllamaEmbeddings(model='llama3.1')
-session_id = st.text_input(label='Session ID', value='Default')
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 
-## Managing the concersation
+## Declaring placeholder for chat history
 if 'store' not in st.session_state:
     st.session_state.store = {}
 
-uploaded_files = st.file_uploader('Upload a PDF file', type='pdf', accept_multiple_files=True)
+## Uploading files for building context
+uploaded_files = st.sidebar.file_uploader('Upload your document to build the context', type='pdf', accept_multiple_files=False)
 
+## creating vector space and retriever for the uploaded file
 if uploaded_files:
 
-    if 'last_uploaded_files' not in st.session_state or st.session_state.last_uploaded_files != uploaded_files:
-        st.session_state.last_uploaded_files = uploaded_files
+    if 'last_uploaded_files' not in st.session_state or st.session_state.last_uploaded_files != uploaded_files.name:
+        st.session_state.last_uploaded_files = uploaded_files.name
 
-        document = []
-        for files in uploaded_files:
-            temp_pdf = f'./temp.pdf'
-            with open(temp_pdf, 'wb') as file:
-                file.write(files.getvalue())
+        ## Deleting older vector space and retriever if a new file is uploaded
+        if 'retriever' in st.session_state:
+            ## Look for a way to reset user input element once a new file is uploaded
+            # del st.session_state.user_input
+            del st.session_state.splits
+            del st.session_state.vector_store
+            del st.session_state.retriever
+ 
+        temp_pdf = f'./temp.pdf'
+        with open(temp_pdf, 'wb') as file:
+            file.write(uploaded_files.getvalue())
 
-            pdf_loader = PyPDFLoader(file_path=temp_pdf)
-            doc = pdf_loader.load()
-            document.extend(doc)
-            os.remove(temp_pdf)
+        pdf_loader = PyPDFLoader(file_path=temp_pdf)
+        doc = pdf_loader.load()
+        os.remove(temp_pdf)
 
-        start_time = time.time()
-        st.session_state.splits = text_splitter.split_documents(documents=document)
+        st.session_state.splits = text_splitter.split_documents(documents=doc)
         st.session_state.vector_store = FAISS.from_documents(documents=st.session_state.splits, embedding=embedng_techn)
         st.session_state.retriever = st.session_state.vector_store.as_retriever()
-        end_time = time.time()
-        print('Time taken for text split, data embedding and retriver creation: ', end_time - start_time)
-
-    else:
-        print('Skipped ')
 
 
-## Tesing if the retriever is created
+## Starting the conversation once the retriever is created
 if 'retriever' in st.session_state:
-
-    print('Started Retriever execution')
 
     ## Prompt for creating a standalone user input if it refere to chat history context
     system_message = (
@@ -97,11 +96,19 @@ if 'retriever' in st.session_state:
     question_answer_chain = create_stuff_documents_chain(model, qa_prompt)
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
+    ## function for managing chat conversation history
     def get_session_history(session_id:str)->BaseChatMessageHistory:
         if session_id not in st.session_state.store:
             st.session_state.store[session_id] = ChatMessageHistory()
-        return st.session_state.store[session_id]
 
+        chat_history = st.session_state.store[session_id]
+        # print(chat_history)
+        if len(chat_history.messages) > 10:
+            chat_history.messages = chat_history.messages[-10:]
+
+        return chat_history
+
+    ## Runnable for chat continuation
     conversational_rag_chain = RunnableWithMessageHistory(
         rag_chain,
         get_session_history,
@@ -110,17 +117,18 @@ if 'retriever' in st.session_state:
         output_messages_key='answer'
     )
 
-    user_input = st.text_input('Ask your question regarding uploaded document.')
-    if user_input:
+    ## accepts user input
+    st.session_state.user_input = st.text_input(label='Ask your question regarding uploaded document.', key='User Input')
+    if st.session_state.user_input:
         session_history = get_session_history(session_id)
         response = conversational_rag_chain.invoke(
-            {'input':user_input},
+            {
+                'input':st.session_state.user_input
+                },
             config={
                 'configurable':{'session_id':session_id}
             }
         )
 
-        st.write(st.session_state.store)
-        st.write('Assistant: ', response['answer'])
-        st.write('Chat history: ', session_history.messages)
+        st.write(response['answer'])
 
